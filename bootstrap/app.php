ca892569f27d16779135b9234df2a1d031f9c73f
timusss0 <mustikavia86@gmail.com>
2025-01-31 00:19:51 +0700

    lumen generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -47,7 +47,7 @@ $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
     App\Console\Kernel::class
 );
-
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
 | Register Config Files
