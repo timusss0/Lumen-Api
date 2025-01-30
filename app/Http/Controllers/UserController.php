269fd7e4579a7a027e55edfd9568580e2916e3c6
timusss0 <mustikavia86@gmail.com>
2025-01-31 01:03:41 +0700

    fiture update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -42,5 +42,28 @@ class UserController extends Controller
         return response()->json($user);
     }
     
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email,' . $id,
+            'age' => 'required|integer|min:18',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
+
+        $user->update($request->all());
+
+        return response()->json($user);
+    }
+
 
 }
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,4 @@ $router->get('/', function () use ($router) {
 $router->get('/users', 'UserController@index');
 $router->post('/users', 'UserController@store');
 $router->get('/users/{id}', 'UserController@show');
+$router->put('/users/{id}', 'UserController@update');
